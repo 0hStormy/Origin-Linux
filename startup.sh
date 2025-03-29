fc9be46783b8712852d5f0340a8568edacc8249b
Stormy <80485413+0hStormy@users.noreply.github.com>
2025-03-29 12:57:22 -0700

    Update Script

--- startup.sh
+++ startup.sh
@@ -1,15 +1,24 @@
+currentgtk=$GTK_THEME
+devmode=true
+
+export GTK_THEME=theme
+
 if ! ping -c 1 "google.com"; then
     swaybg -c '#444444' >/dev/null 2>&1 &
-    env GTK_THEME=origin zenity \
+    zenity \
     --error \
     --text="Origin Linux could not connect to the internet automatically, please set up a network connection by clicking Continue." \
     --title="Network Error" \
     --ok-label="Continue"
-    st nmtui
-    st sh
+    foot nmtui
+    foot sh
 else
+    if [ "$devmode" = true ]; then
+        foot sh &
+    fi
     flask --app linker run &
     chromium-browser --new-window --app=https://origin.mistium.com
 fi
 
+export GTK_THEME=$currentgtk
 exit
